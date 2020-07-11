9af0e3bf833ae33fa210d9c44f4130efeb5cc779
pje00427 <59409769+pje00427@users.noreply.github.com>
2020-07-11 15:47:20 +0900

    Update 04_JOIN_KH계정.sql

--- 04_JOIN_KH계정.sql
+++ 04_JOIN_KH계정.sql
@@ -395,7 +395,17 @@ WHERE E.DEPT_CODE=D.DEPT_ID
     AND D.LOCATION_ID=L.LOCAL_CODE
     AND LOCAL_NAME LIKE 'ASIA%'
     AND JOB_NAME ='대리';
-    
+ 
+--  ANSI구문 
+SELECT E.EMP_ID 사번, E.EMP_NAME 사원명, J.JOB_NAME 직급명, D.DEPT_TITLE 부서명, 
+       L.LOCAL_NAME 근무지역명,E.SALARY 급여
+FROM EMPLOYEE E
+JOIN JOB J USING(JOB_CODE)
+JOIN DEPARTMENT D ON(E.DEPT_CODE = D.DEPT_ID)
+JOIN LOCATION L ON(D.LOCATION_ID = L.LOCAL_CODE)
+WHERE JOB_NAME = '대리' 
+  AND LOCAL_NAME LIKE 'ASIA%';
+
 
 -- 2. 70년대생이면서 여자이고, 성이 전씨인 직원들의
 --    사원명, 주민번호, 부서명, 직급명을 조회하시오
@@ -409,21 +419,60 @@ WHERE E.DEPT_CODE=D.DEPT_ID
     AND SUBSTR(E.EMP_NO,8,1) = 2
     AND EMP_NAME LIKE '전%';
 
+-- ANSI구문
+
+SELECT E.EMP_NAME 사원명, E.EMP_NO 주민번호,D.DEPT_TITLE 부서명,J.JOB_NAME 직급명
+FROM EMPLOYEE E
+JOIN DEPARTMENT D ON(E.DEPT_CODE=D.DEPT_ID)
+JOIN JOB J USING(JOB_CODE)
+WHERE SUBSTR(E.EMP_NO,1,1) = 7
+ AND SUBSTR(E.EMP_NO,8,1) = 2
+ AND EMP_NAME LIKE '전%';
+
+
 -- 3. 이름에 '형'자가 들어있는 직원들의
 --    사번, 사원명, 직급명을 조회하시오
 
-SELECT 
+SELECT E.EMP_NO, E.EMP_NAME, J.JOB_NAME
+FROM EMPLOYEE E,JOB J
+WHERE E.JOB_CODE=J.JOB_CODE
+AND EMP_NAME LIKE '%형%';
 
-
+-- ANSI
+SELECT E.EMP_NO, E.EMP_NAME, J.JOB_NAME
+FROM EMPLOYEE E
+JOIN JOB J USING(JOB_CODE)
+WHERE EMP_NAME LIKE '%형%';
 
 -- 4. 해외영업팀에 근무하는 직원들의
 --    사원명, 직급명, 부서코드, 부서명을 조회하시오
+SELECT E.EMP_NAME 사원명, J.JOB_NAME 직급명, E.DEPT_CODE 부서코드,D.DEPT_TITLE 부서명
+FROM EMPLOYEE E,DEPARTMENT D, JOB J
+WHERE E.DEPT_CODE = D.DEPT_ID
+ AND E.JOB_CODE = J.JOB_CODE
+ AND DEPT_TITLE LIKE '해외영업%';
+ 
+ --ANSI
+ 
+SELECT E.EMP_NAME 사원명, J.JOB_NAME 직급명, E.DEPT_CODE 부서코드,D.DEPT_TITLE 부서명
+FROM EMPLOYEE E
+JOIN DEPARTMENT D ON(E.DEPT_CODE = D.DEPT_ID)
+JOIN JOB J USING(JOB_CODE)
+WHERE DEPT_TITLE LIKE '해외영업%';
 
--- 5. 보너스를 받는 직원들의
+
+-- 5. 보너스를 받는 직원들의 <어려움>
 --    사원명, 보너스, 연봉, 부서명, 근무지역명을 조회하시오
+SELECT E.EMP_NAME 사원명, E.BONUS 보너스, E.SALARY*12 연봉, D.DEPT_TITLE 부서명,L.LOCAL_NAME 근무지역
+FROM EMPLOYEE E,DEPARTMENT D, LOCATION L
+WHERE E.DEPT_CODE=D.DEPT_ID(+)
+ AND D.LOCATION_ID=L.LOCAL_CODE(+)
+ AND E.BONUS IS NOT NULL;
 
 -- 6. 부서가 있는 직원들의
 --    사원명, 직급명, 부서명, 근무지역명을 조회하시오
+
+
 
 -- 7. '한국'과 '일본'에 근무하는 직원들의 
 --    사원명, 부서명, 근무지역명, 근무국가명을 조회하시오
